53c70ed74d5153aa96e1dba9285a863deb99142a
SCI-COPATH <scicopath01@gmail.com>
2021-06-29 19:43:45 +0530

    Add code

---
+++ code/code.ino
@@ -0,0 +1,33 @@
+#include <dht.h>
+#include <LiquidCrystal.h>
+dht DHT;
+#define DHT11_PIN 7
+#define CO2 A0
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+void setup(){
+  lcd.begin(20,4);
+  pinMode(CO2,INPUT);
+}
+
+void loop(){
+  int chk = DHT.read11(DHT11_PIN);
+  lcd.setCursor(0,0); 
+  lcd.print("Temp: ");
+  lcd.print(DHT.temperature);
+  lcd.print((char)223);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print("Humidity: ");
+  lcd.print(DHT.humidity);
+  lcd.print("%");
+  if(digitalRead(CO2)==HIGH){
+    lcd.setCursor(0,3);
+    lcd.print("C02 Detected");
+    delay(700);
+    lcd.setCursor(0,3);
+    lcd.print("              ");
+    delay(700);
+  }else{
+    delay(1000);
+  }
+}
