6a86dba529e8388a94b60e57893148fec3866c26
SCI-COPATH <scicopath01@gmail.com>
2021-06-30 11:30:18 +0530

    Chainge display Size

--- code/code.ino
+++ code/code.ino
@@ -3,21 +3,22 @@
 dht DHT;
 #define DHT11_PIN 6
 #define CO2 A0
-LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup(){
-  lcd.begin(20,4);
+  pinMode(CO2,INPUT);
+  lcd.begin(16,4);
   pinMode(CO2,INPUT);
 }
 
 void loop(){
   int chk = DHT.read11(DHT11_PIN);
   lcd.setCursor(0,0); 
-  lcd.print("Temp: ");
+  lcd.print("Temp:");
   lcd.print(DHT.temperature);
   lcd.print((char)223);
   lcd.print("C");
   lcd.setCursor(0,1);
-  lcd.print("Humidity: ");
+  lcd.print("Humidity:");
   lcd.print(DHT.humidity);
   lcd.print("%");
   if(digitalRead(CO2)==HIGH){
