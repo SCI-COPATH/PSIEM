06d3cbe58e58f3fa20fd19e05375c6314002594b
SCI-COPATH <scicopath01@gmail.com>
2021-07-01 12:53:17 +0530

    fix bug add circuit v 1.1

--- code/code.ino
+++ code/code.ino
@@ -1,34 +1,53 @@
+#include "DHT.h"
 #include <dht.h>
+#define DHTPIN 6 
 #include <LiquidCrystal.h>
-dht DHT;
+#define DHTTYPE DHT11 
 #define DHT11_PIN 6
+DHT dht(DHTPIN, DHTTYPE);
 #define CO2 A0
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
 void setup(){
-  pinMode(CO2,INPUT);
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
   lcd.begin(16,4);
   pinMode(CO2,INPUT);
+  dht.begin();
 }
 
 void loop(){
-  int chk = DHT.read11(DHT11_PIN);
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+   // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
   lcd.setCursor(0,0); 
   lcd.print("Temp:");
-  lcd.print(DHT.temperature);
+  lcd.print(t);
   lcd.print((char)223);
   lcd.print("C");
   lcd.setCursor(0,1);
   lcd.print("Humidity:");
-  lcd.print(DHT.humidity);
+  lcd.print(h);
   lcd.print("%");
   if(digitalRead(CO2)==HIGH){
     lcd.setCursor(0,3);
     lcd.print("C02 Detected");
-    delay(700);
+    delay(100);
     lcd.setCursor(0,3);
     lcd.print("              ");
-    delay(700);
+    delay(100);
   }else{
-    delay(1000);
+    delay(2000);
   }
 }
